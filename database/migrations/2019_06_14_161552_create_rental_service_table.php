b0b976503c6f121d78ac63519c3382322087e611
taichow <w.lafdili@gmail.com>
2019-06-14 18:20:29 +0200

    created rental_service table

--- database/migrations/2019_06_14_161552_create_rental_service_table.php
+++ database/migrations/2019_06_14_161552_create_rental_service_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateServicesTable extends Migration
+class CreateRentalServiceTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateServicesTable extends Migration
      */
     public function up()
     {
-        Schema::create('services', function (Blueprint $table) {
+        Schema::create('rental_service', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('description');
+            $table->bigInteger('rental_id')->unsigned()->index();
+            $table->bigInteger('service_id')->unsigned()->index();
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateServicesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('services');
+        Schema::dropIfExists('rental_service');
     }
 }
