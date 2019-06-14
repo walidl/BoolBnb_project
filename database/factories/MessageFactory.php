a45e744cf27ee28051b8576e0bc14e84666eb3fb
Sara <fornonisara9@gmail.com>
2019-06-14 12:36:46 +0200

    Changes in migration/model/factory of Users and Messages

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -5,8 +5,9 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Message::class, function (Faker $faker) {
     return [
-        //
+        'content' => $faker->text($maxNbChars = 200) ,
+        'sent_date' => $faker->date,
     ];
 });
