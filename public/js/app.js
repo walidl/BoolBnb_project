becebfa9b33bb345d62492a43f7505d1f1bdf354
Sara <fornonisara9@gmail.com>
2019-06-24 11:11:41 +0200

    commit message

--- public/js/app.js
+++ public/js/app.js
@@ -49361,8 +49361,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/Airbnb/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/Airbnb/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/BoolBnb_project/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/BoolBnb_project/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,3 +10,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/rentals/all', 'RentalController@showRentals')->name('rental.show-all');
 Route::get('/rentals/new', 'RentalController@createRental')->name('rental.create');
 Route::post('/rentals', 'RentalController@storeRental')->name('rental.store');
+Route::get('/inbox/{id}', 'MessagesController@printMessagesById')->name('printMess')->middleware('auth');
